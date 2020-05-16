8488957aa835be746b315126b0024388fbdc92db
georgeo <georgeosabu@gmail.com>
2020-05-16 11:38:10 +0200

    getting the mean and normalizing

--- pca.cpp
+++ pca.cpp
@@ -1,7 +1,29 @@
-#include "Eigen/src/Eigenvalues/EigenSolver.h"
-#include "Eigen/src/Eigenvalues/SelfAdjointEigenSolver.h"
+#include "Eigen/Dense"
 #include "values.hpp"
-
+#include <iostream>
+using namespace Eigen;
+using namespace std;
 int main(int argc,char* argv[]){
+    double janMean;
+    double juneMean;
+
+    for (int i = 0; i < january.size(); i++)
+    {
+        janMean+=january[i];
+        juneMean+=june[i];
+
+    }
+    janMean=janMean/(january.size());
+    juneMean=juneMean/(june.size());
+
+    for (int i = 0; i < january.size(); i++)
+    {
+        january[i]=january[i]-janMean;
+        june[i]=june[i]-juneMean;
+    }
     
+    cout<<janMean<<"  "<<january[0]<<"  "<<juneMean<<endl;
+    
+    MatrixXd m(2,2);     
+
 }
